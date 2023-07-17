bf0fe23970afae2eddb3be3aeb349891dd007539
Karol Zlot <11590960+karolzlot@users.noreply.github.com>
2023-07-17 04:32:34 +0200

    Use jinja2 template

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from dotenv import load_dotenv
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+from routers import router
 
 load_dotenv()
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-
-from routers import router
 
 app.include_router(router)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    from microwave import Microwave
+
+    microwave = Microwave()
+    microwave_data = await microwave.get_microwave_data()
+
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "microwave_data": microwave_data}
+    )
+
 
 if __name__ == "__main__":
     import uvicorn
